fd70697185772610476b5e82d8e8e0c7217c8fb7
pramodtarpe <72032932+pramodtarpe@users.noreply.github.com>
2021-07-18 22:48:07 +0530

    fix bugs

--- selection.js
+++ selection.js
@@ -4,21 +4,26 @@ async function selectionSort(){
         var min_index = i;
         let val1 = parseInt(bars[i].style.height);
         for(let j=i+1;j<bars.length;j++){
+            bars[j].style.backgroundColor = "orange";
             let val2 = parseInt(bars[j].style.height);
             if(val2 < val1){
+                if(min_index != i){
+                    bars[min_index].style.backgroundColor = "red";
+                }
                 min_index = j;
                 val1 = val2;
             }
-            bars[j].style.backgroundColor = "orange";
             await new Promise((resolve) =>
                 setTimeout(() => {
                 resolve();
                 }, 300)
             );
             bars[j].style.backgroundColor = "red";
+            bars[min_index].style.backgroundColor = "blue";
         }
         if(i != min_index){
             swap(bars[i],bars[min_index]);
+            bars[min_index].style.backgroundColor = "red";
         }
         await new Promise((resolve) =>
             setTimeout(() => {
